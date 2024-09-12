5cbee5323836669ba86e110939916cc359e88a8d
alunoantonion <antonion4000@gmail.com>
2024-09-12 20:24:52 -0300

    12/09

---
+++ js/newFile.js
@@ -0,0 +1,12 @@
+if (n == ".") {
+
+    bt_nr = bt_nr.contact(n);
+}
+else if (bt) bt_nr.c_nr;
+[0] !== "0";
+SS$$;
+bt_nr[1] !== ".";
+{
+    bt_nr = ontact(n);
+
+}
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,24 @@
-$(document).ready(function() { 
+$(document).ready(function( { 
+    var nr_c = "";
 
-});
+    $(".bt-nr").click ()}
+    {
+        var n = $(this).text();
+
+        if(bt_nr !== "0") }
+        
+
+ {
+                if(bt_nr.length == 1 $$ n == ".") }{
+                    bt_nr = bt_nr.contact(n)
+                    $(this).attr("disabled", "true");
+         } else {
+            
+
+        
+        bt_nr = bt_nr.contact(n)
+
+        $("txt_display").val(bt_nr)
+    
+
+}); 
